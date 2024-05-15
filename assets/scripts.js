75106046d440049c5968430177bb12a708cd2d78
Mahammedi Abdelghani <wboductqr@relay.firefox.com>
2024-05-15 14:31:23 +0100

    add  Route for managers to send a click
    1.  `/managers/send` (POST): Route for managers to send a click
    2.  route and view for manger dashboard (super simple view)

--- assets/scripts.js
+++ assets/scripts.js
@@ -7,10 +7,34 @@ function resetOccurrence(managerId) {
     .then((message) => {
       // Display the response message as a  popout
       alert(message);
+      location.reload();
     })
     .catch((error) => {
       console.error('Error resetting occurrence:', error);
       // Handle error
       alert('An error occurred while resetting occurrence');
+    });
+}
+
+function sendMessage(managerId = 1) {
+  fetch('/manager/send', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      managerId: managerId,
+    }),
+  })
+    .then((response) => response.text())
+    .then((message) => {
+      // Display the response message as a  popout
+      alert(message);
+      location.reload();
+    })
+    .catch((error) => {
+      console.error('Error sending message:', error);
+      // Handle error
+      alert('An error occurred while sending message');
     });
 }
---
+++ routes/manager.routes.js
@@ -0,0 +1,56 @@
+const Manager = require('../models/Manager');
+module.exports = function (app) {
+  app.get('/manager/dashboard', (req, res) => {
+    res.render('manager/dashboard');
+  });
+  // Reset occurrence route
+  app.post('/manager/send', async (req, res) => {
+    try {
+      const manager = await Manager.findOne({
+        where: { id: req.body.managerId },
+      });
+      console.log('response___>' + manager.occurrence);
+
+      if (!manager) {
+        return res
+          .status(404)
+          .render('error', { message: 'Manager not found' });
+      }
+      const currentDate = new Date();
+      const currentHour = currentDate.getHours();
+      console.warn(currentHour >= 10 && manager.occurrence === 0);
+      if (currentHour >= 10 && manager.occurrence === 0) {
+        console.log('check');
+        await Manager.update(
+          { occurrence: 1 },
+          { where: { id: req.body.managerId } }
+        );
+        return res.send('Your message has been sent to  your administrator !');
+      }
+      if (manager.occurrence === 1) {
+        await Manager.update(
+          { occurrence: 0 },
+          { where: { id: req.body.managerId } }
+        );
+        return res.send('Your message has been sent to  your administrator !');
+      } else {
+        const nextResetTime = new Date(
+          currentDate.getFullYear(),
+          currentDate.getMonth(),
+          currentDate.getDate() + 1,
+          10,
+          0,
+          0
+        );
+        return res.send(
+          `Sorry, you have to wait until ${nextResetTime.toLocaleString()} for your next click or contact your administrator.`
+        );
+      }
+    } catch (err) {
+      console.error(err);
+      return res
+        .status(500)
+        .render('error', { message: 'Internal server error' });
+    }
+  });
+};
